ea93fcd491807614d99ad27a620311e13726a5d2
Abdidawud <abdidawud995@gmail.com>
2025-08-02 20:59:14 +0300

    update 2

---
+++ .next/static/chunks/app_dashboard_layout_tsx_ea6e06e0._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_48849090._.js",
+  "static/chunks/[next]_internal_font_google_323c5c77._.css"
+],
+    source: "dynamic"
+});
